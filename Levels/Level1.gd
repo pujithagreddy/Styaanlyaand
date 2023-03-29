494e261ffcc03c801f120d58497fd01dd0b58304
pujithagreddy <beatrixbbx0411@outlook.com>
2023-03-29 07:32:03 +0530

    Made necessary copies of NPCS and other changes

--- Levels/Level1.gd
+++ Levels/Level1.gd
@@ -4,10 +4,10 @@ func _ready():
 	if Global.door_name:
 		var door_node = find_node(Global.door_name)
 		if door_node:
-			$YSort/MainPlayer.position.x = 376
-			$YSort/MainPlayer.position.y = 2008
+			$MainPlayer.position.x = 376
+			$MainPlayer.position.y = 2008
 
 
 func _process(delta):
-	if $"CanvasLayer/black".modulate.a>=0:
-		$"CanvasLayer/black".modulate.a-= 1.5*delta
+	if $CanvasLayer/black.modulate.a>=0:
+		$CanvasLayer/black.modulate.a-= 1.5*delta
